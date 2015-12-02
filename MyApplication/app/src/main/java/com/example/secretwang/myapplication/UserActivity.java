6384c659df4c9c7b34e374b5dfc1393b263ad99e
iosdevelop123 <543626290@qq.com>
2015-12-02 18:42:36 +0800

    11

--- MyApplication/app/src/main/java/com/example/secretwang/myapplication/UserActivity.java
+++ MyApplication/app/src/main/java/com/example/secretwang/myapplication/UserActivity.java
@@ -2,12 +2,21 @@ package com.example.secretwang.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserActivity extends AppCompatActivity {
-
+    private Button button_back = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+       button_back=(Button)findViewById(R.id.btn_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 }
