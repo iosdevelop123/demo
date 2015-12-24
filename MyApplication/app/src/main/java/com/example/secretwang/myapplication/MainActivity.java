647d32e2210c85e0166dd87e5cbdb6b5b6983ce7
iosdevelop123 <543626290@qq.com>
2015-12-24 13:35:06 +0800

    返回键增加弹窗

--- MyApplication/app/src/main/java/com/example/secretwang/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/secretwang/myapplication/MainActivity.java
@@ -921,49 +921,23 @@ public class MainActivity extends Activity {
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         if (keyCode == KeyEvent.KEYCODE_BACK && event.getRepeatCount() == 0) {
-            onBackPressed();
-            timer.cancel();
+            new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("是否退出登录？")
+                    .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            //退出登录
+                            timer.cancel();
+                            finish();
+                            onBackPressed();
+                        }
+                    })
+                    .setNegativeButton("取消", null)
+                    .setCancelable(false)
+                    .show();
             return true;
         }
         return super.onKeyDown(keyCode, event);
     }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        client.connect();
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "Main Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.example.secretwang.myapplication/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.start(client, viewAction);
-//    }
-//
-//    @Override
-//    public void onStop() {
-//        super.onStop();
-//
-//        // ATTENTION: This was auto-generated to implement the App Indexing API.
-//        // See https://g.co/AppIndexing/AndroidStudio for more information.
-//        Action viewAction = Action.newAction(
-//                Action.TYPE_VIEW, // TODO: choose an action type.
-//                "Main Page", // TODO: Define a title for the content shown.
-//                // TODO: If you have web page content that matches this app activity's content,
-//                // make sure this auto-generated web page URL is correct.
-//                // Otherwise, set the URL to null.
-//                Uri.parse("http://host/path"),
-//                // TODO: Make sure this auto-generated app deep link URI is correct.
-//                Uri.parse("android-app://com.example.secretwang.myapplication/http/host/path")
-//        );
-//        AppIndex.AppIndexApi.end(client, viewAction);
-//        client.disconnect();
-//    }
 }
